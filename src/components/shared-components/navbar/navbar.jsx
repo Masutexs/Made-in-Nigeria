72e6f68e260d57f362ab15236245b695f37acb91
InimfonUdoh <okoninimfon2001@gmail.com>
2023-10-04 08:36:27 -0700

    Added an updated on responsiveness

--- src/components/shared-components/navbar/navbar.jsx
+++ src/components/shared-components/navbar/navbar.jsx
@@ -4,14 +4,15 @@ import { Outlet, Link } from 'react-router-dom'
 function Navbar() {
   return (
     <div>
-      <nav className='text-black py-8 shadow mb-4 flex gap-4 justify-center'>
+      <nav className="w-[1514px] h-[53px] bg-black">
+      <img className="w-[78px] h-[83px]" src="https://via.placeholder.com/78x83" />
       <Link to={'/'}>Home</Link>
       <Link to={'/about'}>About</Link>
       <Link to={'/contact'}>Contact</Link>
       <Link to={'/computer'}>Computer and accessories</Link>
     
       </nav>
-
+     
       <Outlet />
     </div>
   )
