6df80289692e3e2f110e0ac4238571a6d3efc609
kemetim <etimaniekeme991@gmail.com>
2023-10-11 07:26:05 -0700

    created cosmetic page

---
+++ src/pages/landing-pages/cosmetic.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { items } from '../../components/shared-components/Items/itemData'
+import Layout from '../../../components/shared-components/layout/layout'
+import Button from '../../components/shared-components/'
+
+function Cosmetic() {
+  return (
+    <div>C</div>
+  )
+}
+
+export default Cosmetic
